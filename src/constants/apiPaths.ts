fb8da0d2474aaa9164b097b48a411285272210b9
Ilja Gusiatin <igoose@macbook-pro>
2025-03-13 14:23:49 +0200

    feat: update API URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://d0abicxxid.execute-api.us-east-1.amazonaws.com",
+  product: "https://l6oh5lemt2.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://9ao3gxc6kd.execute-api.us-east-1.amazonaws.com",
+  import: "https://2h651i2p8j.execute-api.us-east-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
