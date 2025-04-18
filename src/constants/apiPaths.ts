b54a7b9eb8b68052362e7f3fcf951998ec535130
Ilja Gusiatin <igoose@macbook-pro>
2025-04-18 16:23:16 +0300

    feat: task10 integrate BFF

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://sjo479bbl3.execute-api.us-east-1.amazonaws.com",
   order: "https://drnbvxpa60yn9.cloudfront.net/api",
   import: "https://7otr9lrwoi.execute-api.us-east-1.amazonaws.com",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://d1ibjwnye2fljh.cloudfront.net",
   cart: "https://drnbvxpa60yn9.cloudfront.net/api",
 };
 
